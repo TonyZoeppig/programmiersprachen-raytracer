d9c294042fcf55c518eefb7f2c4303bfce051af3
TonyZoeppig <tonyjan98@gmail.com>
2020-07-14 18:49:36 +0200

    5.4 shape print method + overload << operator

--- source/shape.cpp
+++ source/shape.cpp
@@ -1,5 +1,17 @@
 #include "shape.hpp"
+#include <iostream>
 
 Shape::Shape(std::string name, glm::vec3 const& color) :
 	name_{name},
 	color_{color} {}
+
+std::ostream& Shape::print(std::ostream& os) const
+{
+	os << "Name: " << name_ << ", Color (RGB): " << color_.x << " | " << color_.y << " | " << color_.z;
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, Shape const& s)
+{
+	return s.print(os);
+}
--- source/shape.hpp
+++ source/shape.hpp
@@ -10,9 +10,12 @@ public:
 	Shape(std::string name, glm::vec3 const& color_);
 	virtual float area() const = 0;
 	virtual float volume() const = 0;
+	virtual std::ostream& print(std::ostream& os) const;
 protected:
 	std::string name_ = "Shape";
 	glm::vec3 color_{ 128, 128, 128 };
 };
+
+std::ostream& operator<<(std::ostream& os, Shape const& s);
 
 #endif
